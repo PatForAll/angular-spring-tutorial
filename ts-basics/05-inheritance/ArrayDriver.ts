4416cce97f2b0b10f5c63d3d172386aa30deedce
PatForAll <siekmeier.p@gmail.com>
2021-11-15 18:09:41 +0100

    implement array for inherited class

--- ts-basics/05-inheritance/ArrayDriver.ts
+++ ts-basics/05-inheritance/ArrayDriver.ts
@@ -3,10 +3,16 @@ import {Shape} from "./Shape";
 import {Rectangle} from "./Rectangle";
 
 let shape: Shape = new Shape(5, 5);
-console.log(shape.getInfo());
-
 let circle: Circle = new Circle(0, 10, 4);
-console.log(circle.getInfo());
-
 let rectangle: Rectangle = new Rectangle(10, 0, 5, 6);
-console.log(rectangle.getInfo());
+
+let shapes: Shape[] = [];
+shapes.push(shape);
+shapes.push(circle);
+shapes.push(rectangle);
+
+shapes.push(new Shape(10, 10));
+
+for (let item of shapes) {
+    console.log(item.getInfo());
+}
