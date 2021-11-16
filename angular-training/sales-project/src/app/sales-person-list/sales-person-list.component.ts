f4d9e7c64dc5d8c1e5ec378cfe9319b776fc3d3b
PatForAll <siekmeier.p@gmail.com>
2021-11-16 15:09:18 +0100

    add basic sales-person-list

---
+++ angular-training/sales-project/src/app/sales-person-list/sales-person-list.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import {SalesPerson} from "./sales-person";
+
+@Component({
+  selector: 'app-sales-person-list',
+  templateUrl: './sales-person-list.component.html',
+  styleUrls: ['./sales-person-list.component.css']
+})
+export class SalesPersonListComponent implements OnInit {
+
+  // create an array of objects
+  salesPersonList: SalesPerson[] = [
+    new SalesPerson('Anup', 'Kumar', 'anup.kumar@luv2code.com', 50000),
+    new SalesPerson('John', 'Doe', 'jd@unknown.org', 10000),
+    new SalesPerson('Mai', 'Truong', 'mt@somemail.net', 25000)
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
